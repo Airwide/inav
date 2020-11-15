f83b72793bd9ebe3c42cd1bc221bac356448804b
Airwide <arvid.stenberg@gmail.com>
2020-11-15 10:39:29 +0100

    Fixed bug introduced in last commit

--- src/main/navigation/navigation_fixedwing.c
+++ src/main/navigation/navigation_fixedwing.c
@@ -76,7 +76,7 @@ static int8_t loiterDirYaw = 1;
 static float getSmoothnessCutoffFreq(float baseFreq)
 {
     uint16_t smoothness = 10 - navConfig()->fw.control_smoothness;
-    return 0.001f * baseFreq * (float)(smoothness*smoothness*smoothness) + 0.01f;
+    return 0.001f * baseFreq * (float)(smoothness*smoothness*smoothness) + 0.1f;
 }
 
 // Calculates the cutoff frequency for smoothing out pitchToThrottleCorrection
@@ -85,7 +85,7 @@ static float getSmoothnessCutoffFreq(float baseFreq)
 static float getPitchToThrottleSmoothnessCutoffFreq(float baseFreq)
 {
     uint16_t smoothness = 10 - navConfig()->fw.pitch_to_throttle_smooth;
-    return 0.001f * baseFreq * (float)(smoothness*smoothness*smoothness) + 0.1f;
+        return 0.001f * baseFreq * (float)(smoothness*smoothness*smoothness) + 0.01f;
 }
 
 /*-----------------------------------------------------------
